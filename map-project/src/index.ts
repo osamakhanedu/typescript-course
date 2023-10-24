b959b16d2c7cd04d31c485c59d528e748cf078aa
osamakhanedu <osamakhanedu@gmail.com>
2023-10-24 11:48:51 +0500

    added map

--- map-project/src/index.ts
+++ map-project/src/index.ts
@@ -1,9 +1,20 @@
+/// <reference types="@types/google.maps" />
+
 import { User} from './User';
 import { Company  } from "./Company";
 
+const elementRef = document.getElementById("map") as HTMLElement;
+
+const map = new google.maps.Map(elementRef,{
+    zoom:1,
+    center: { lat:  0, lng: 0 },
+});
 
 
-const user = new User()
-const company = new Company();
-console.log(user);
-console.log(company);
+
+// const user = new User()
+// const company = new Company();
+// console.log(user);
+// console.log(company);
+
+// console.log(google);
