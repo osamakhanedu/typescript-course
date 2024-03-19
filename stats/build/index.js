b82e26e4c636884a3fd2707abf75a238a64347d6
osamakhanedu <osamakhanedu@gmail.com>
2024-03-19 11:55:55 +0500

    working with file loading

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const console_1 = require("console");
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', { encoding: 'utf-8' }).split('\n').map((line) => line.split(','));
+// log(content)
+let winCount = 0;
+for (const match of matches) {
+    if (match[1] === "Man United" && match[5] === 'H') {
+        winCount++;
+    }
+}
+(0, console_1.log)('win count ' + winCount);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world');
+import { log } from 'console';
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv',{ encoding: 'utf-8'}).split('\n').map((line: string): string[]=> line.split(','));
+
+// log(content)
+
+let winCount = 0;
+
+
+for (const match of matches) {
+
+    if(match[1]=== "Man United" && match[5] === 'H'){
+        winCount++;
+    }
+}
+
+log('win count ' + winCount)
