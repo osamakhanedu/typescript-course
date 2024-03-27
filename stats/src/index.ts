5a9eab0bcf9b6a26a23b82e4424dabc6ee42faca
osamakhanedu <osamakhanedu@gmail.com>
2024-03-27 21:40:40 +0500

    created enum

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,16 +1,25 @@
 import { log } from 'console';
 import fs from 'fs';
 
-const matches = fs.readFileSync('football.csv',{ encoding: 'utf-8'}).split('\n').map((line: string): string[]=> line.split(','));
+const matches = fs.readFileSync('football.csv', { encoding: 'utf-8' }).split('\n').map((line: string): string[] => line.split(','));
 
-// log(content)
+
+// enum - enumeration 
+// set of very closely related value ( constant )
+enum MatchResult {
+    HomeWin = 'H',
+    AwayWin = 'A',
+    Draw = 'D'
+};
 
 let winCount = 0;
 
 
 for (const match of matches) {
 
-    if(match[1]=== "Man United" && match[5] === 'H'){
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+        winCount++;
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
         winCount++;
     }
 }
